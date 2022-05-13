7d7bf86db542ad8d8e6ae0ba6c0791522eca84f4
Mine-Creative <aimannababan@gmail.com>
2022-05-13 23:29:47 +0700

    Kembali ke point sebelumnya
    Karena adanya error

--- js/script.js
+++ js/script.js
@@ -35,3 +35,16 @@ btn.addEventListener("click", function () {
     localStorage.setItem("theme", theme);
 });
 
+const progressBar = document.querySelector('#progress-bar');
+const section= document.querySelector('section');
+
+const animateProgress = () => {
+    let scrollDistance = -section.getBoundingClientRect().top;
+    let progressWidth = 
+        (scrollDistance / (section.getBoundingClientRect().height - document.documentElement.clientHeight)) * 100;
+    let value = Math.floor(progressWidth);
+    progressBar.style.width = value + '%';
+};
+
+window.addEventListener('scroll', animateProgress);
+
